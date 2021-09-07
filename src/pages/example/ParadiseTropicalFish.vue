a6623356649807c066796e9a5a06ca7e8ed03777
Ritusan <37280671+Ritusan@users.noreply.github.com>
2021-09-07 14:44:31 +0800

    Update ParadiseTropicalFish.vue

--- src/pages/example/ParadiseTropicalFish.vue
+++ src/pages/example/ParadiseTropicalFish.vue
@@ -1,41 +1,17 @@
 <template>
-  <div>
-    <div class="one-color-wrap">
-      <div v-for="(item,index) in one" :key="index">
-        <div v-if="item.id !== 0" class="one-color-item" :style="{'background-color': item.color}">
-          <span>{{item.name}}</span>
-          <span>{{item.color}}</span>
-        </div>
-      </div>
-    </div>
-    <div class="two-color-wrap type1">
-      <div class="two-color-item" 
-        v-for="(item,index) in two1" :key="index"
-        :style="{'background-color': item.color2}">
-        <div :style="{'background-color': item.color1}">
-          <span :style="{'color': item.color2}">{{item.color1}}</span>
-        </div>
-        <div :style="{'background-color': item.color2}">{{item.color2}}</div>
-      </div>
-    </div>
-    <div class="two-color-wrap type2">
-      <div class="two-color-item"
-        v-for="(item,index) in two2" :key="index">
-        <div :style="{'background': item.bgColor}">{{item.color}}</div>
-      </div>
-    </div>
-    <div class="two-color-wrap type3">
-      <div class="two-color-item"
-        v-for="(item,index) in two3" :key="index"
-        :style="{'background-color': item.color}">
-        <div :style="{'background-image': item.bgImage}">{{item.color}}</div>
-      </div>
-    </div>
+  <div class="color-example">
+    <color-example 
+      :one="one"
+      :two1="two1"
+      :two2="two2"
+      :two3="two3">
+    </color-example>
   </div>
 </template>
 
 <script>
 
+import ColorExample from "@/components/ColorExample"
 import {oneList,twoListType1,twoListType2,twoListType3} from "@/utils/colorful/paradiseTropicalFish"
 
   export default {
@@ -47,6 +23,9 @@ import {oneList,twoListType1,twoListType2,twoListType3} from "@/utils/colorful/p
         two3: twoListType3
       }
     },
+    components: {
+      ColorExample
+    },
     mounted(){
 
     }
@@ -54,5 +33,5 @@ import {oneList,twoListType1,twoListType2,twoListType3} from "@/utils/colorful/p
 </script>
 
 <style lang="scss" scoped>
-@import "@/styles/colorful.scss";
+
 </style>
