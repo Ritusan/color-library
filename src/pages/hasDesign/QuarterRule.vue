d96ab0997a03b0b32217f0cbfc8f4c35189ba8c2
Akira <1323109714@qq.com>
2021-09-24 01:41:02 +0800

    add quarter rule

---
+++ src/pages/hasDesign/QuarterRule.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="has-design quarter-rule">
+    <div class="left-box">
+      <span>北欧</span>
+      <span>大阪</span>
+      <span class="letter">ART</span>
+      <span class="letter">PROJECT</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'QuarterRule'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/hasDesign/QuarterRule.scss";
+</style>
