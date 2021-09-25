b9d586553898b24e64e75ba70394589883205346
Akira <1323109714@qq.com>
2021-09-25 14:43:31 +0800

    add tilt effect

---
+++ src/pages/hasDesign/TiltEffect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="has-design tilt-effect">
+    <div class="tilt-effect-content">
+      <div class="line-one">
+        <span>HAPPY BIBLEE</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TiltEffect'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/hasDesign/TiltEffect.scss";
+</style>
