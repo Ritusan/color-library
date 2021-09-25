7f955bfd2e613be16b321815732693253dce8c6f
Akira <1323109714@qq.com>
2021-09-25 15:01:03 +0800

    add SmallDecorations

---
+++ src/pages/hasDesign/SmallDecorations.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="has-design small-decorations">
+    <div class="small-decorations-content">
+      <div class="line-one">
+        <span>HAPPY</span>
+        <span>BIBLEE</span>
+      </div>
+    </div>
+    <div class="top-text">
+      <span>AKIRA</span>
+    </div>
+    <div class="right-text">
+      <span>HAPPY</span>
+    </div>
+    <div class="bottom-text">
+      <span>AKIRA</span>
+    </div>
+    <!-- <div class="left-text">
+      <span>HAPPY</span>
+    </div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SmallDecorations'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/hasDesign/SmallDecorations.scss";
+</style>
