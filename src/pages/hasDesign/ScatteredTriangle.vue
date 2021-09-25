88ab49bc15412a13cf387d60ecdd465791331104
Akira <1323109714@qq.com>
2021-09-25 12:40:15 +0800

    update package.json

---
+++ src/pages/hasDesign/ScatteredTriangle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="has-design scattered-triangle">
+    <div class="triangle-box-wrap">
+      <div class="triangle-box">
+        <span>GATHER</span>
+        <span>PICNIC</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ScatteredTriangle'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/hasDesign/ScatteredTriangle.scss";
+</style>
