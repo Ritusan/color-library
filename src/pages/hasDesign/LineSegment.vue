71e872f6479eecd7650913f16fb33616987681ee
Akira <1323109714@qq.com>
2021-09-25 13:45:53 +0800

    add line segment

---
+++ src/pages/hasDesign/LineSegment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="has-design line-segment">
+    <div class="line-segment-content">
+      <div class="line-one">
+        <div class="line-one-wrap">
+          <span>2021/09/25</span>
+          <span class="split-line">|</span>
+          <span>TAKE FREE</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LineSegment'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/hasDesign/LineSegment.scss";
+</style>
