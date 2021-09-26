6bd7d49ffac3187c69840bc9f65e1c3fd62fb86c
Akira <1323109714@qq.com>
2021-09-26 11:45:59 +0800

    add DialogIntimacy

---
+++ src/pages/hasDesign/DialogIntimacy.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="has-design dialog-intimacy">
+    <!-- <div class="content flex-row-between">
+      <span>HAPPY</span>
+      <span>Polaris</span>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DialogIntimacy',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/DialogIntimacy.scss';
+</style>
