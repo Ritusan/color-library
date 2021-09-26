b825059e39cc85db961ab33b1ba6c3555dfcfe12
Akira <1323109714@qq.com>
2021-09-26 13:55:21 +0800

    add HollowoutWords

---
+++ src/pages/hasDesign/HollowoutWords.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="has-design hollowout-words">
+    <div class="content flex-column">
+      <span>是自由的时代</span>
+      <span>HAPPY</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HollowoutWords',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/HollowoutWords.scss';
+</style>
