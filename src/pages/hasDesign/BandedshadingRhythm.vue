bcc678b957da0daf6db03c2890695ee7611398cc
Akira <1323109714@qq.com>
2021-09-26 11:41:44 +0800

    add BandedshadingRhythm

---
+++ src/pages/hasDesign/BandedshadingRhythm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="has-design bandedshading-rhythm">
+    <div class="content flex-row-between">
+      <span>HAPPY</span>
+      <span>Polaris</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BandedshadingRhythm',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/BandedshadingRhythm.scss';
+</style>
