f808a4b8cc60f730086871475780bbeb06253394
Akira <1323109714@qq.com>
2021-09-26 08:44:01 +0800

    add AddThickline

---
+++ src/pages/hasDesign/AddThickline.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="has-design add-thickline">
+    <div class="content flex-column">
+      <div class="line flex-column">
+        <span>HAPPY</span>
+        <span>Polaris</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddThickline',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/AddThickline.scss';
+</style>
