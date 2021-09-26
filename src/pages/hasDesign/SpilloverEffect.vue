8518c25720fd679741c18f7a1eeab5048002dc11
Akira <1323109714@qq.com>
2021-09-26 13:02:35 +0800

    add SpilloverEffect

---
+++ src/pages/hasDesign/SpilloverEffect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="has-design spillover-effect">
+    <div class="content">
+      <span>HAPPY</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpilloverEffect',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/SpilloverEffect.scss';
+</style>
