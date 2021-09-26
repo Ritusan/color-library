2ea42b219945dd7898d4bc520fddf2d3c5a451f0
Akira <1323109714@qq.com>
2021-09-26 08:32:45 +0800

    add WordsBlank

---
+++ src/pages/hasDesign/UseIllustrations.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="has-design use-illustrations">
+    <div class="content flex-column">
+      <!-- <span>HAPPY</span>
+      <span>Polaris</span> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UseIllustrations',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/UseIllustrations.scss';
+</style>
---
+++ src/pages/hasDesign/WordsBlank.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="has-design words-blank">
+    <div class="line flex-row-between">
+      <span>H</span>
+      <span>A</span>
+      <span>P</span>
+      <span>P</span>
+      <span>Y</span>
+    </div>
+    <div class="line flex-row-between">
+      <span>A</span>
+      <span>K</span>
+      <span>I</span>
+      <span>R</span>
+      <span>A</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordsBlank',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/WordsBlank.scss';
+</style>
