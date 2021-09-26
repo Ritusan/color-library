2afc50e647fceda1db8835a67fad16880ff8521d
Akira <1323109714@qq.com>
2021-09-26 11:28:25 +0800

    add TextBorder

---
+++ src/pages/hasDesign/TextBorder.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="has-design text-border">
+    <div class="content flex-column">
+      <span class="border">HAPPY</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextBorder',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/TextBorder.scss';
+</style>
