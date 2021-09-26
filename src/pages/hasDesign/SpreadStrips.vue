1e61069c35d21eb7ba9b80778398b49e8b11280e
Akira <1323109714@qq.com>
2021-09-26 13:46:30 +0800

    add SpreadStrips

---
+++ src/pages/hasDesign/SpreadStrips.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="has-design spread-strips">
+    <div class="strips">
+      <span>HAPPY</span>
+      <span>Polaris</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpreadStrips',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/SpreadStrips.scss';
+</style>
