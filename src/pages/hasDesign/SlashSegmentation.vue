452e2e18d40442d90fc7d90b35ca4a90e18f3369
Akira <1323109714@qq.com>
2021-09-27 13:03:27 +0800

    add SlashSegmentation

---
+++ src/pages/hasDesign/SlashSegmentation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="has-design slash-segmentation">
+    <div class="content flex-column">
+      <span>HAPPY</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlashSegmentation',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/hasDesign/SlashSegmentation.scss';
+</style>
